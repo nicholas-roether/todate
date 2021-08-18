e0640469743b5110a706d8799c003f4c3db633e9
nicholas-roether <nicholas.roether@t-online.de>
2021-08-18 13:04:46 +0200

    add more mutations

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -7,6 +7,8 @@ import Database from "../../src/db";
 import User from "../../src/models/user";
 import GraphQLDate from "graphql-date";
 
+// TODO relocate this code
+
 const typeDefs = gql`
 	scalar Date
 
@@ -45,18 +47,48 @@ const typeDefs = gql`
 
 	type Mutation {
 		createReminder(
-			time: String!,
-			title: String,
-			description: String,
-			wholeDay: Boolean,
+			time: String!
+			title: String
+			description: String
+			wholeDay: Boolean
 			notificationOffsets: [Int!]
 			categoryId: ID
-		): Reminder
+		): Reminder!
 		createCategory(
-			name: String!,
+			name: String!
 			icon: String
 			expandByDefault: Boolean
-		): Category
+		): Category!,
+		categorizeReminder(
+			reminderId: ID!
+			categoryId: ID!
+		): Reminder!
+		addNotificationOffset(
+			reminderId: ID!
+			offset: Int!
+		): Reminder!
+		removeNotificationOffset(
+			reminderId: ID!
+			offset: Int!
+		): Reminder!
+		setNotificationOffsets(
+			reminder: ID!
+			offsets: [Int!]!
+		): Reminder!
+		updateReminder(
+			id: ID!
+			title: String
+			description: String
+			dueAt: Date
+			duration: Int
+			wholeDay: Boolean
+		): Reminder!
+		updateCategory(
+			id: ID!,
+			name: String,
+			icon: String,
+			expandByDefault: Boolean
+		)
 	}
 `;
 
@@ -121,7 +153,7 @@ const resolvers = {
 		}
 	},
 	Mutation: {
-		async createReminder(_, { time, title, description, wholeDay, notificationOffsets, categoryId }, { session }, { fieldNodes }) {
+		async createReminder(_, { time, title, description, wholeDay, notificationOffsets, categoryId }, { session }) {
 			if(!session) throw Error("Not logged in");
 			const db = await Database.get();
 			const doc = new db.Reminder({
@@ -133,10 +165,10 @@ const resolvers = {
 				category: categoryId,
 				owner: User.fromSession(session).id
 			});
-			await doc.save().catch(err => { throw new Error(`Failed to save reminder: ${err}`) });
+			await doc.save();
 			return doc;
 		},
-		async createCategory(_, { name, icon, expandByDefault }, { session }, { fieldNodes }) {
+		async createCategory(_, { name, icon, expandByDefault }, { session }) {
 			if(!session) throw Error("Not logged in");
 			const db = await Database.get();
 			const doc = new db.Category({
@@ -145,7 +177,82 @@ const resolvers = {
 				expandByDefault,
 				owner: User.fromSession(session).id
 			});
-			await doc.save().catch(err => { throw new Error(`Failed to save category: ${err}`) });
+			await doc.save();
+			return doc;
+		},
+		async categorizeReminder(_, { reminderId, categoryId }, { user }) {
+			if(!user) throw Error("Not logged in");
+			const db = await Database.get();
+			if(!(await db.Category.exists({ _id: categoryId}))) throw Error("Category doesn't exist");
+			const doc = await db.Reminder.findById(reminderId).exec();
+			if(!doc || !user.idMatches(doc.get("owner"))) throw Error("Reminder does not exist or you don't have access to it");
+			doc.set("category", categoryId);
+			await doc.save();
+			return doc;
+		},
+		async addNotificationOffset(_, { reminderId, offset }, { user }) {
+			if(!user) throw Error("Not logged in");
+			const db = await Database.get();
+			const doc = await db.Reminder.findById(reminderId).exec();
+			if(!doc || !user.idMatches(doc.get("owner"))) throw Error("Reminder does not exist or you don't have access to it");
+			doc.get("notificationOffsets").push(offset);
+			await doc.save();
+			return doc;
+		},
+		async removeNotificationOffset(_, { reminderId, offset }, { user }) {
+			if(!user) throw Error("Not logged in");
+			const db = await Database.get();
+			const doc = await db.Reminder.findById(reminderId).exec();
+			if(!doc || !user.idMatches(doc.get("owner"))) throw Error("Reminder does not exist or you don't have access to it");
+			const offsets = doc.get("notificationOffsets");
+			while(offsets.includes(offset)) offsets.splice(offsets.firstIndexOf(offset), 1);
+			doc.set("notificationOffsets", offsets);
+			await doc.save();
+			return doc;
+		},
+		async setNotificationOffsets(_, { reminderId, offsets }, { user }) {
+			if(!user) throw Error("Not logged in");
+			const db = await Database.get();
+			const doc = await db.Reminder.findById(reminderId).exec();
+			if(!doc || !user.idMatches(doc.get("owner"))) throw Error("Reminder does not exist or you don't have access to it");
+			doc.set("notificationOffsets", offsets);
+			await doc.save();
+			return doc;
+		},
+		async updateReminder(_, {
+			id,
+			title,
+			description,
+			dueAt,
+			duration,
+			wholeDay
+		}, { user }) {
+			if(!user) throw new Error("Not logged in");
+			const db = await Database.get();
+			const doc = await db.Reminder.findById(id).exec();
+			if(!doc || !user.idMatches(doc.get("owner"))) throw Error("Reminder does not exist or you don't have access to it");
+			if(title) doc.set("title", title);
+			if(description) doc.set("description", description);
+			if(dueAt) doc.set("dueAt", dueAt);
+			if(duration) doc.set("duration", duration);
+			if(wholeDay) doc.set("wholeDay", wholeDay);
+			await doc.save();
+			return doc;
+		},
+		async updateCategory(_, {
+			id,
+			name,
+			icon,
+			expandByDefault
+		}, { user }) {
+			if(!user) throw new Error("Not logged in");
+			const db = await Database.get();
+			const doc = await db.Category.findById(id).exec();
+			if(!doc || !user.idMatches(doc.get("owner"))) throw Error("Category does not exist or you don't have access to it");
+			if(name) doc.set("name", name);
+			if(icon) doc.set("icon", icon);
+			if(expandByDefault) doc.set("expandByDefault", expandByDefault);
+			await doc.save();
 			return doc;
 		}
 	}
