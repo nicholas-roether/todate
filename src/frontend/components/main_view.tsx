8ae235a4383faf70f41760a5f6057409a7af7dae
nicholas_roether <nicholas.roether@t-online.de>
2021-09-07 08:49:07 +0200

    remove unused code

--- src/frontend/components/main_view.tsx
+++ src/frontend/components/main_view.tsx
@@ -29,28 +29,6 @@ const MainView = ({ startDate, page, onUpdatePage }: MainViewProps) => {
 	const classes = useStyles();
 	return (
 		<div className={classes.container}>
-			{/* <ResizableContainer
-				right
-				defaultWidth={350}
-				minWidth={160}
-				maxWidth="min(600px, max(calc(100% - 800px), 160px))"
-			>
-				<Drawer
-					variant="permanent"
-					open
-					className={classes.drawer}
-					PaperProps={{ className: classes.drawerPaper }}
-				>
-					<Clock />
-				</Drawer>
-			</ResizableContainer>
-			<div style={{ flex: "1" }}>
-				<CalendarView
-					startDate={startDate}
-					page={page}
-					onUpdatePage={onUpdatePage}
-				/>
-			</div> */}
 			<CalendarSidebar
 				startDate={startDate}
 				page={page}
